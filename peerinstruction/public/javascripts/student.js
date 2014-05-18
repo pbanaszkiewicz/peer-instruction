e8b79c4cfee976b9a6a608066ffffcf52590e5b9
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2014-05-18 18:35:33 +0200

    Updated student's JS

--- peerinstruction/public/javascripts/student.js
+++ peerinstruction/public/javascripts/student.js
@@ -1,7 +1,6 @@
 window.onload = function() {
 
     var room, localStream
-    var streams = []
 
     $.get("/roomid", {roomName: "classroom"}, function(res) {
         room = res;
@@ -26,16 +25,14 @@ window.onload = function() {
         $.getJSON("/enroll", {username: username, room: room}, function(data) {
             room = Erizo.Room({token: data.token})
 
-            localStream.addEventListener("access-accepted", function() {
-                console.log("Access accepted!")
+            console.log("Joining the room!")
 
-                // if there's anything left in the div for video stream
-                // $("#videoStream").empty()
+            localStream.addEventListener("access-accepted", function() {
+                console.log("Access to the stream granted!")
 
                 room.addEventListener("room-connected", function(roomEvent) {
                     console.log("Room connected!")
 
-                    // room.publish(localStream)
                     subscribeToStreams(roomEvent.streams)
                 })
 
@@ -46,34 +43,26 @@ window.onload = function() {
                     var div = document.createElement("div")
                     div.setAttribute("style", "height: 120px; width: 160px")
                     div.setAttribute("id", "smallerStream" + stream.getID())
-                    document.getElementById("smallerStreams").appendChild(div)
+                    document.getElementById("videoStream").appendChild(div)
                     stream.show("smallerStream" + stream.getID())
                 })
 
                 room.addEventListener("stream-added", function(event) {
-                    console.log("Stream added!")
-                    streams.push(event.stream)
-                    subscribeToStreams(streams)
-                    ///
-                    if (localStream.getID() === event.stream.getID()) {
-                        console.log("Your stream has been published!");
-                    }
+                    console.log("New stream available!")
+                    subscribeToStreams([event.stream])
                 })
 
                 room.addEventListener("stream-removed", function(event) {
-                    // TODO: this function doesn't remove streams from global
-                    // `streams` variable.  Might cause troubles :<
                     var stream = event.stream
                     if (stream.elementID !== undefined)
                     {
                         var element = document.getElementById(stream.elementID)
-                        document.body.removeChild(element)
+                        element.remove()
                     }
                     console.log("Removed stream ", stream.getID())
                 })
 
                 room.connect()
-                localStream.show("videoStream")
             })
 
             localStream.init()
